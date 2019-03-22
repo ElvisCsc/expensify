ef4a0b30ae784f03204bc3ddfb5661d1e19acfa5
ElvisCsc <33323318+ElvisCsc@users.noreply.github.com>
2019-03-22 15:11:35 +0200

    Setup production build and server

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ import './Styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({description: 'Water bill', amount: 4500}));
+/*store.dispatch(addExpense({description: 'Water bill', amount: 4500}));
 store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000}));
 store.dispatch(addExpense({ description: 'Rent', amount: 1000}));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
+console.log(visibleExpenses);*/
 
 const jsx = (
     <Provider store={store}>
